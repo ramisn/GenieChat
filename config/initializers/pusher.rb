bacd94384f348fb1882ec703598e8a76040cd634
Ramesh <rameshkror@gmail.com>
2016-03-16 12:30:15 +0530

    first commit

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,3 +1,6 @@
-Pusher.app_id = ENV['PUSHER_APP_ID']
-Pusher.key = ENV['PUSHER_KEY']
-Pusher.secret = ENV['PUSHER_SECRET']
+# Pusher.app_id = ENV['PUSHER_APP_ID']
+# Pusher.key = ENV['PUSHER_KEY']
+# Pusher.secret = ENV['PUSHER_SECRET']
+Pusher.app_id = '84732'
+Pusher.key = "6e870fcf26fe80ff383f"
+Pusher.secret = "d06cb542a6d938b04cae"
