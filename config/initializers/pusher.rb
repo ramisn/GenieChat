628005fc4176d63d61c038745f9e69ff71e74ffa
Ramesh K <rameshkror@gmail.com>
2016-03-24 21:03:06 +0530

    Update pusher.rb

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,6 +1,6 @@
 # Pusher.app_id = ENV['PUSHER_APP_ID']
 # Pusher.key = ENV['PUSHER_KEY']
 # Pusher.secret = ENV['PUSHER_SECRET']
-Pusher.app_id = '84732'
-Pusher.key = "6e870fcf26fe80ff383f"
-Pusher.secret = "d06cb542a6d938b04cae"
+Pusher.app_id = '847322'
+Pusher.key = "6e870fcf26fe80ff383ff"
+Pusher.secret = "d06cb542a6d938b04caee"
